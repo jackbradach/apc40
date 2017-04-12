b23cfcb6aaa3ac3eb03bea321e18d7ce13f04eb9
Jack Bradach <jack@bradach.net>
2017-04-11 23:44:17 -0700

    Updated with more wooshy effects; Knob code is kind of bodged in there, but it looks cool enough for tonight!

--- apc40.py
+++ apc40.py
@@ -31,14 +31,28 @@ class Knob():
         # track is mapped to midi channel
         chan = self.CHAN_BASE + track
         self._m_out.send_message([chan, self._knob_ctrl_id, v])
-        self._position = v
+        self._position[track] = v
 
 
     def set_led_ring_type(self, track, v):
         chan = self.CHAN_BASE + track
         self._m_out.send_message([chan, self._led_ctrl_id, v])
 
+    def add_saturate(self, track, v):
+        if (track < 0 or track > 8):
+            raise ValueError
+        pos = self._position[track] + v
+        if pos > 127:
+            pos = 127
+        self.set_position(track, pos)
 
+    def sub_saturate(self, track, v):
+        if (track < 0 or track > 8):
+            raise ValueError
+        pos = self._position[track] - v
+        if pos < 0:
+            pos = 0
+        self.set_position(track, pos)
 
 class Button():
     """Button types can be multicolor, unicolor, or plain"""
@@ -125,32 +139,49 @@ if __name__ == "__main__":
     dc_knobs = []
     tc_knobs = []
 
+    # random '53,73' range is to try to center around 127/2 (with some variation)
+    # This makes the random dial animations look cooler.
     for i in range(0, 8):
         dck = Knob(apc._m_out, 0x10 + i)
-        dck.set_led_ring_type(0, 2)
+        dck.set_led_ring_type(0, 3)
+        dck.set_position(0, random.randint(53, 73))
         dc_knobs.append(dck)
 
         tck = Knob(apc._m_out, 0x30 + i)
-        tck.set_led_ring_type(0, 2)
+        tck.set_led_ring_type(0, 1)
+        tck.set_position(0, random.randint(53, 73))
         tc_knobs.append(tck)
 
-    dc_pos = 0
-    tc_pos = 127
+    idx = 0
+    step = 8
     while(1):
         for x in range(0, 8):
             for y in range(0, 5):
                 apc.clip_launch_led(x+1,y+1,random.randint(1,6) | 1)
 
         for k in dc_knobs:
-            k.set_position(0, dc_pos)
-        dc_pos += 9
-        if (dc_pos > 127):
-            dc_pos = 0
+            if (k._position[0] > 63):
+                k.add_saturate(0, step)
+            else:
+                k.sub_saturate(0, step)
+            if k._position[0] <= 0 or k._position[0] >= 127:
+                k.set_position(0, random.randint(53, 73))
 
         for k in tc_knobs:
-            k.set_position(0, tc_pos)
-        tc_pos -= 9
-        if (tc_pos < 0):
-            tc_pos = 127
+            if (k._position[0] > 63):
+                k.add_saturate(0, step)
+            else:
+                k.sub_saturate(0, step)
 
+            if k._position[0] <= 0 or k._position[0] >= 127:
+                k.set_position(0, random.randint(53, 73))
+
+
+        # idx += step
+        # if idx > (127 + step):
+        #     for k in dc_knobs:
+        #         k.set_position(0, random.randint(0, 127))
+        #     for k in tc_knobs:
+        #         k.set_position(0, random.randint(0, 127))
+        #     idx = 0
         sleep(.1)
