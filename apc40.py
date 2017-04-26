ffc1a8bb4f438f8164bd165bb14afdf7a625f46b
Jack Bradach <jack@bradach.net>
2017-04-25 19:13:19 -0700

    Cleanup and refactoring; no functional difference.

--- apc40.py
+++ apc40.py
@@ -7,7 +7,9 @@ from time import sleep
 
 TRACK_COUNT = 8
 
+
 class Knob():
+
     CHAN_BASE = 0xB0
 
     """Knobs have positions indicated by the LEDs"""
@@ -53,6 +55,7 @@ class Knob():
             pos = 0
         self.set_position(track, pos)
 
+
 class Button():
     """Simple button"""
     def __init__(self, m_out, note):
@@ -70,12 +73,12 @@ class Button():
         self._is_on = v
 
     def on(self):
-        n = 0x90 + self._note # 0x90 == MIDI 'note on'
+        n = 0x90 + self._note  # 0x90 == MIDI 'note on'
         self._send([self._channel, n, 0])
         self._is_on = True
 
     def off(self, v):
-        n = 0x80 + self._note # 0x80 == MIDI 'note_off'
+        n = 0x80 + self._note  # 0x80 == MIDI 'note_off'
         self._send([self._channel, n, 0])
         self._is_on = False
 
@@ -88,8 +91,10 @@ class Slider():
     def value(self):
         return None
 
+
 class TrackControl():
     BASE_ID = 0x30
+
     def __init__(self, m_out):
 
         # Track control has 8 knobs...
@@ -108,60 +113,64 @@ class Activator():
     def __init__(self):
         pass
 
+
 class SoloCue():
     def __init__(self):
         pass
 
+
 class RecordArm():
     def __init__(self):
         pass
+
 
 class ClipLaunchGrid():
     def __init__(self):
         self._buttons = []
 
 #        for (i in range(0, 8)): # 8 tracks
-
-
-
 #        for (i in range(0, 4)):
 #            self._buttons.append(Button(m_out, 0x57))
-
-        pass
-
-
-#class ClipStopBar():
+# class ClipStopBar():
 #    pass
 #
-#class SceneLaunchBar():
+# class SceneLaunchBar():
+
 
 class APC40():
     CLIP_LAUNCH_1 = 0x35
 
-    def __init__(self):
-        self._m_in = rtmidi.MidiIn()
-        self._m_out = rtmidi.MidiOut()
+    def __init__(self, fake_hw=False):
+        self._init_midi(fake_hw)
 
-        # Look up the input/output port IDs for the APC40
+    def _init_midi(self, fake_hw=False):
+        """Initializes the MIDI backend (optionally a stubbed out one
+        for testing).  For real hardware, the APC40 will be detected
+        and queried or an exception thrown."""
+        if (fake_hw):
+            self._m_in = None
+            self._m_out = None
+        else:
+            self._m_in = rtmidi.MidiIn()
+            self._m_out = rtmidi.MidiOut()
+            (port_in, port_out) = self._find_port_ids()
+            self._m_in.open_port(port_in)
+            self._m_out.open_port(port_out)
+            self._m_in.set_callback(self)
+
+    def _find_port_ids(self):
         try:
-            self.p_in = \
+            port_in = \
                 [i for i, s in enumerate(self._m_in.get_ports())
                     if 'APC40' in s][0]
-            self.p_out = \
+            port_out = \
                 [i for i, s in enumerate(self._m_out.get_ports())
                     if 'APC40' in s][0]
-        except:
-            print("APC40 not found!")
-            return None
+        except IndexError:
+            print("APC40 port ID not found on MIDI bus!")
+            raise
 
-        # Open the ports
-        self._m_in.open_port(self.p_in)
-        self._m_out.open_port(self.p_out)
-
-        self._m_in.set_callback(self)
-
-
-
+        return (port_in, port_out)
 
     def __call__(self, event, data=None):
         message, deltatime = event
@@ -186,8 +195,8 @@ if __name__ == "__main__":
     dc_knobs = []
     tc_knobs = []
 
-    # random '53,73' range is to try to center around 127/2 (with some variation)
-    # This makes the random dial animations look cooler.
+    # random '53,73' range is to try to center around 127/2
+    # (with some variation) This makes the random dial animations look cooler.
     for i in range(0, 8):
         dck = Knob(apc._m_out, 0x10 + i)
         dck.set_led_ring_type(0, 3)
@@ -204,7 +213,7 @@ if __name__ == "__main__":
     while(1):
         for x in range(0, 8):
             for y in range(0, 5):
-                apc.clip_launch_led(x+1,y+1,random.randint(1,6) | 1)
+                apc.clip_launch_led(x+1, y+1, random.randint(1, 6) | 1)
 
         for k in dc_knobs:
             if (k._position[0] > 63):
@@ -222,7 +231,6 @@ if __name__ == "__main__":
 
             if k._position[0] <= 0 or k._position[0] >= 127:
                 k.set_position(0, random.randint(53, 73))
-
 
         # idx += step
         # if idx > (127 + step):
